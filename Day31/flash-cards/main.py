b24308a2885fced7ffbba4f0c083ff6eaaf62722
Nick Pawsey <pawsey18@gmail.com>
2021-03-04 19:49:07 -0400

    day 32

--- Day31/flash-cards/main.py
+++ Day31/flash-cards/main.py
@@ -3,10 +3,18 @@ import pandas
 import random
 
 BACKGROUND_COLOR = "#B1DDC6"
-
-data = pandas.read_csv('data/french_words.csv')
-to_learn = data.to_dict(orient='records')
 current_card = {}
+to_learn = {}
+try:
+
+    data = pandas.read_csv('data/words_to_learn.csv')
+except FileNotFoundError:
+    original_data = pandas.read_csv('data/french_words.csv')
+    to_learn = original_data.to_dict(orient='records')
+else:
+    to_learn = data.to_dict(orient='records')
+    to_learn = data.to_dict(orient='records')
+
 
 
 def next_card():
@@ -24,6 +32,13 @@ def flip_card():
     canvas.itemconfig(card_title, text="English", fill='white')
     canvas.itemconfig(card_text, text=current_card['English'], fill='white')
     canvas.itemconfig(card_image_background, image=card_back_img)
+
+
+def is_known():
+    to_learn.remove(current_card)
+    data = pandas.dataFrame(to_learn)
+    data.to_csv('data/words_to_learn.csv', index=False)
+    next_card()
 
 
 window = Tk()
@@ -47,7 +62,7 @@ unknown_button = Button(image=cross_image, highlightthickness=0, command=next_ca
 
 unknown_button.grid(row=1, column=0)
 check_image = PhotoImage(file='images/right.png')
-known_button = Button(image=check_image, highlightthickness=0, command=next_card)
+known_button = Button(image=check_image, highlightthickness=0, command=is_known)
 known_button.grid(row=1, column=1)
 
 next_card()
