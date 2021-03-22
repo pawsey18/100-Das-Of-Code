3dfb8290b268707f42c29c1b60f6224fad255263
Nick Pawsey <pawsey18@gmail.com>
2021-03-22 10:26:15 -0300

    100 movies that you must watch exercise complete code3

--- Day45/web scraping with beautiful soup/main.py
+++ Day45/web scraping with beautiful soup/main.py
@@ -1,8 +1,18 @@
+import requests
 from bs4 import BeautifulSoup
 
-import requests
+URL = "https://www.empireonline.com/movies/features/best-movies-2/"
 
-response = requests.get('https://news.ycombinator.com/news')
+response = requests.get(URL)
+website_html = response.text
 
-print(response.text)
+soup = BeautifulSoup(website_html, "html.parser")
 
+all_movies = soup.find_all(name="h3", class_="title")
+
+movie_titles = [movie.getText() for movie in all_movies]
+movies = movie_titles[::-1]
+
+with open("movies.txt", mode="w") as file:
+    for movie in movies:
+        file.write(f"{movie}\n")
